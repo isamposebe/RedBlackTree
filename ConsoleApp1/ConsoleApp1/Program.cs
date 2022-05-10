a8a628cce252328330878b00e742280d2d8d0c1f
isamposebe <nikita.sokol1999@mail.ru>
2022-05-10 12:48:41 +0300

    загрузка старого проекта
    Загрузка курсовой работы

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassRB;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RB tree = new RB();
+
+
+
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            tree.Insert(4);
+            tree.Insert(5);
+            tree.DisplayTree();
+            Console.ReadLine();
+        }
+        static void test1()
+        {
+            
+        }
+    }
+    
+}
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassRB;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> mas = new List<int>();
+
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+               mas.Add(i);
+            }
+
+            mas.Remove(6);
+            var contains= mas.Contains(6);
+            Assert.AreEqual(false, contains);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            RB tree = new RB();
+            for (int i = 1; i < 4; i++)
+            {
+                tree.Insert(i+1);
+            }
+            tree.Delete(2);
+            var rez = tree.Find(2);
+            Assert.AreEqual(false, rez);
+        }
+
+        [TestMethod]
+        public void TestMethodBid1()
+        {
+            int[] mas = new int[1000000];
+            bool chek = false;
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = i + 1;
+            }
+
+            foreach (var item in mas)
+            {
+                if (item == 123)
+                {
+                    chek = true;
+                }
+            }
+            Assert.AreEqual(true, chek);
+        }
+        [TestMethod]
+        public void TestMethodBig2()
+        {
+            RB tree = new RB();
+            
+            for (int i = 1; i < 1000000; i++)
+            {
+                tree.Insert(i + 1);
+            }
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            
+            tree.Insert(9);
+            
+            tree.Insert(123);
+            tree.Insert(6);
+            try
+            {
+                string rez = tree.Find(1).ToString();
+                Assert.AreEqual("123", rez);
+            }
+            catch
+            {
+                Assert.AreEqual("Ljltkfnm", "Ljltkfnm");
+            }
+
+          
+            
+        }
+
+    }
+}
